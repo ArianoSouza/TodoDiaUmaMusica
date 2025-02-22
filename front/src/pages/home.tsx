dd06324a5d47e459c18f459c7e5c489e806bb174
Ariano <arianosouzapro@gmail.com>
2025-02-22 14:49:50 -0300

    ajustando deploy da api #4

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './home.module.css'
 
 export default function Home() {
-    interface Musica {
-        id: number;
-        titulo: string;
-        artista: string;
-      }
+    interface Musica{
+      id:Number,
+      nome:String,
+      artista:String
+    }
 
-    const [musicas, setMusicas] = useState();
+    const [musicas, setMusicas] = useState<Musica>();
 
     useEffect(() => {
         axios.get('https://tododiaumamusica.onrender.com/musicas')
@@ -26,9 +26,10 @@ export default function Home() {
     return (
     <>
     <div className={styles.container}>
-        <img src="" alt="" />
+        <img src= { musicas?.id == 6 ? "https://upload.wikimedia.org/wikipedia/en/b/b0/Brockhampton_-_Saturation.png": ""} alt="capa" />
         <h2 className={styles.titulo}>{musicas?.nome}</h2>
         <p className={styles.artist}>{musicas?.artista}</p>
+        <a href={musicas?.id == 6 ? 'https://www.youtube.com/watch?v=94nuoYQORDw':""} target="_blank">{musicas?.id == 6 ? "https://www.youtube.com/watch?v=94nuoYQORDw" : ""}</a>
     </div>
     </>
         
